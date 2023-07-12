c2449b7e4c2a8fed1635928494ad968d9d0e56b5
LuisComZ <luizmay2@gmail.com>
2023-07-11 21:57:30 -0300

    shuffle and reveal cards script

--- src/js/game.js
+++ src/js/game.js
@@ -1,4 +1,16 @@
 const grid = document.querySelector(".grid")
+const characters = [
+  "beth",
+  "jerry",
+  "jessica",
+  "morty",
+  "pessoa-passaro",
+  "pickle-rick",
+  "rick",
+  "summer",
+  "meeseeks",
+  "scroopy",
+]
 
 function createElement(tag, className) {
   const element = document.createElement(tag) //cria um elemento
@@ -6,15 +18,38 @@ function createElement(tag, className) {
   return element
 }
 
-function createCard() {
+function revealCard({ target }) {
+  target.parentNode.classList.add("reveal-card")
+
+  if (target.parentNode.className.includes("reveal-card")){
+    return
+  }
+}
+
+function createCard(character) {
   const card = createElement("div", "card") //define o elemento e sua respectiva classe alterando os parâmetros da função
-  const front = createElement("div", "front")
-  const back = createElement("div", "back")
+  const front = createElement("div", "face front")
+  const back = createElement("div", "face back")
+
+  front.style.backgroundImage = `url(../src/images/${character}.png)`
 
   card.appendChild(front) //adiciona uma div filha dentro da div mãe
   card.appendChild(back)
 
+  card.addEventListener("click", revealCard)
+
   return card
 }
 
-createCard()
+function loadGame() {
+  const duplicateCharacters = [...characters, ...characters] //pega os elementos do array characters e espalha 2 vezes dentro do array duplicateCharacters
+
+  const shuffledArray = duplicateCharacters.sort(() => Math.random() - 0.5)
+
+  duplicateCharacters.forEach(function (character) {
+    const card = createCard(character)
+    grid.appendChild(card)
+  })
+}
+
+loadGame()
